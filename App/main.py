0bc079083d75b3758010bc897658dcb81b4ce795
Jonathan <jonathan2lozere@gmail.com>
2025-04-19 14:26:33 +0200

    Add a MainWindow and a join_button

---
+++ App/main.py
@@ -0,0 +1,26 @@
+import sys
+from PySide6.QtWidgets import QApplication, QMainWindow, QPushButton, QVBoxLayout, QWidget
+
+class MainWindow(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        self.join_button = QPushButton("Join")
+
+        self.centralWidget = QWidget()
+        self.setCentralWidget(self.centralWidget)
+        self.layout = QVBoxLayout()
+        self.layout.addWidget(self.join_button)
+        self.centralWidget.setLayout(self.layout)
+
+        self.join_button.clicked.connect(self.join_game)
+    
+    def join_game(self):
+        pass
+
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+
+    window = MainWindow()
+    window.show()
+
+    sys.exit(app.exec())
