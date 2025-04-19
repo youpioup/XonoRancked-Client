07de90c40ecae60e7b095b5593c31aa78e0f8102
Jonathan <jonathan2lozere@gmail.com>
2025-04-19 14:55:00 +0200

    Add join_game functionality to MainWindow

--- App/main.py
+++ App/main.py
@@ -1,5 +1,7 @@
 import sys
 from PySide6.QtWidgets import QApplication, QMainWindow, QPushButton, QVBoxLayout, QWidget
+import subprocess
+
 
 class MainWindow(QMainWindow):
     def __init__(self):
@@ -15,7 +17,7 @@ class MainWindow(QMainWindow):
         self.join_button.clicked.connect(self.join_game)
     
     def join_game(self):
-        pass
+        subprocess.run(["xonotic-sdl-wrapper", "+connect 192.168.1.155:26000"])
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
