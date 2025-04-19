d0d207f8953f186c444f55b5c3c84f2ecaee4738
Jonathan <jonathan2lozere@gmail.com>
2025-04-19 17:05:56 +0200

    Wait slots available before join a game

--- App/main.py
+++ App/main.py
@@ -1,5 +1,6 @@
 import sys
 from PySide6.QtWidgets import QApplication, QMainWindow, QPushButton, QVBoxLayout, QWidget
+from PySide6.QtCore import QTimer
 import subprocess
 import requests
 
@@ -14,18 +15,45 @@ class MainWindow(QMainWindow):
         self.layout = QVBoxLayout()
         self.layout.addWidget(self.join_button)
         self.centralWidget.setLayout(self.layout)
+        self.timer = QTimer()
 
-        self.join_button.clicked.connect(self.join_game)
+        self.join_button.clicked.connect(self.start_search)
+        self.timer.timeout.connect(self.join)
     
-    def join_game(self):
+    def start_search(self):
+        self.join_button.setText("Wait for a game")
+        self.timer.start(5000)
+
+
+    def join(self):
+        if self.slots_avalible() > 0:
+            self.join_game(self.get_server())
+            self.timer.stop()
+            self.join_button.setText("Join")
+
+    def get_server(self):
         url = "http://127.0.0.1:8000/server/0"
-
         res = requests.get(url)
-
         if res.status_code == 200:
-            print(res.json())
+            port = res.json()["port"]
+            return port
+        else:
+            print(f"error:{res.status_code}")
+        
 
-        subprocess.run([f"xonotic-sdl-wrapper", "+connect 192.168.1.155:{res.json[port]}"])
+
+    def slots_avalible(self):
+        r = requests.get("http://127.0.0.1:8000/server_status/0")
+        if r.status_code == 200:
+            slots = r.json()["slot_free"]
+            return slots
+        else:
+            print (f"error:{r.status_code}")
+
+
+    def join_game(self, port:int):
+
+        subprocess.run(["xonotic-sdl-wrapper", f"+connect 192.168.1.155:{port}"])
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
